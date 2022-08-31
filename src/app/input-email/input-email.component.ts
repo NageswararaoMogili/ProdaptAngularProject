af194be4812572e143a462d870b78bf834677a0f
My Name <nageswararao.m@prodapt.com>
2022-08-31 22:25:14 +0530

    Nagi Initial commit

---
+++ src/app/input-email/input-email.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-input-email',
+  templateUrl: './input-email.component.html',
+  styleUrls: ['./input-email.component.css']
+})
+export class InputEmailComponent implements OnInit {
+
+  @Input() item = '';
+  @Output() newItemEvent = new EventEmitter<string>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+   addNewItem(value: string) {
+    this.newItemEvent.emit(value);
+  }
+  
+}
