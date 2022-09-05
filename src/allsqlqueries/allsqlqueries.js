22fbe0313cbaaa14971e26af02bc2de0afcd3d3a
MogiliNag <nageswararao.m@prodapt.com>
2022-09-05 12:34:15 +0530

    update

--- src/allsqlqueries/allsqlqueries.js
+++
@@ -1,4 +0,0 @@
-exports.finduserbyphoneno = "SELECT * FROM users WHERE phone=?";
-exports.addnewuser = "INSERT INTO users set ?";
-exports.finduserbyid = "SELECT * FROM users WHERE id=?";
-exports.updateuserbyid = `UPDATE users SET firstname=?, lastname=?,birthday=?,gender=?,email=?,phone=?,username=?,password=? WHERE id=?`;
--- src/routes/users.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const userController = require('../controllers/users.controller');
-// Create a new user
-router.post('/adduser', userController.create);
-router.get("/finduser/:id", userController.findById)
-router.post("/updateuser/:id", userController.update)
-module.exports = router
