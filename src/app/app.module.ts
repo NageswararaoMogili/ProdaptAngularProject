6579008dc7b253362587a09a1382f4f3db27cc21
Mogili4744 <nageswararao.m@prodapt.com>
2022-09-21 17:03:57 +0530

    buttoncomponent created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InputTextComponent } from './input-text/input-text.component';
-import { InputEmailComponent } from './input-email/input-email.component';
 import { HeaderComponent } from './header/header.component';
 import { DropdownComponent } from './dropdown/dropdown.component';
 import { LoginComponent } from './login/login.component';
@@ -13,18 +12,19 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { NotfoundComponent } from './notfound/notfound.component';
 import { GlobalService } from './global.service';
 import { HttpClientModule } from '@angular/common/http';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InputTextComponent,
-    InputEmailComponent,
     HeaderComponent,
     DropdownComponent,
     LoginComponent,
     SignupComponent,
     DashboardComponent,
     NotfoundComponent,
+    ButtonComponent,
 
   ],
   imports: [
--- src/app/button/button.component.spec.ts
+++ src/app/button/button.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { InputEmailComponent } from './input-email.component';
+import { ButtonComponent } from './button.component';
 
-describe('InputEmailComponent', () => {
-  let component: InputEmailComponent;
-  let fixture: ComponentFixture<InputEmailComponent>;
+describe('ButtonComponent', () => {
+  let component: ButtonComponent;
+  let fixture: ComponentFixture<ButtonComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ InputEmailComponent ]
+      declarations: [ ButtonComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(InputEmailComponent);
+    fixture = TestBed.createComponent(ButtonComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
