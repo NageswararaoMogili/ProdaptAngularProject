c91127fcd3424215705162df61e36f2d679ac871
Mogili4744 <nageswararao.m@prodapt.com>
2022-09-27 22:53:21 +0530

    Reactive inputs

--- src/app/reactive-input/reactive-input.component.ts
+++ src/app/reactive-input/reactive-input.component.ts
@@ -1,36 +1,87 @@
-import { AfterViewInit, Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { AfterViewInit, Component, OnInit, Input, Output, EventEmitter, ElementRef, HostBinding, forwardRef } from '@angular/core';
 import { ControlValueAccessor , NG_VALUE_ACCESSOR} from '@angular/forms';
 import { InputTypes } from 'src/inputs';
 
 @Component({
   selector: 'app-reactive-input',
   templateUrl: './reactive-input.component.html',
-  styleUrls: ['./reactive-input.component.css']
+  styleUrls: ['./reactive-input.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => ReactiveInputComponent),
+      multi: true,
+    },
+  ]
 })
 export class ReactiveInputComponent 
-implements OnInit, ControlValueAccessor, AfterViewInit  {
+implements ControlValueAccessor, AfterViewInit  {
+  
+  @HostBinding('class.llac-input') reactiveInputClass = true;
 
   @Input() type: InputTypes = 'text';
   @Input() placeholder = '';
+  @Input() labelname = '';
   @Input() required = true;
+  @Input() disabled = false;
+  @Input() readonly = false;
+  @Input() minLength?: number = 0;
+  @Input() maxLength?: number = 999;
+  @Input() requiredText? = '';
+  @Input() patternChange = '';
+  @Input() errorTextColor? = false;
+  @Input() id = '';
+  value = '';
+  @Input() classname? = '';
+
+  onChange!: (value: string) => void;
+  onTouched!: () => void;
+  qmAttribute: any;
   
 
-  constructor() { }
+  constructor(private readonly el: ElementRef) {}
 
-  writeValue(obj: any): void {
-    throw new Error('Method not implemented.');
+  /**
+   * @param value - string
+   * It will write the value we want to the formControl
+   */
+   writeValue(value: string): void {
+    this.value = value;
   }
-  registerOnChange(fn: any): void {
-    throw new Error('Method not implemented.');
+
+  /**
+   * @param fn - The callback function to register
+   * When the value changes in the UI, call the registered
+   * function to allow the forms API to update itself:
+   */
+  registerOnChange(fn: VoidFunction): void {
+    this.onChange = fn;
   }
-  registerOnTouched(fn: any): void {
-    throw new Error('Method not implemented.');
+
+  /**
+   * @param fn - The callback function to register
+   * On blur (or equivalent), your class should call the registered function to allow
+   * the forms API to update itself:
+   */
+  registerOnTouched(fn: VoidFunction): void {
+    this.onTouched = fn;
   }
-  setDisabledState?(isDisabled: boolean): void {
-    throw new Error('Method not implemented.');
-  }
+
+  // writeValue(obj: any): void {
+  //   throw new Error('Method not implemented.');
+  // }
+  // registerOnChange(fn: any): void {
+  //   throw new Error('Method not implemented.');
+  // }
+  // registerOnTouched(fn: any): void {
+  //   throw new Error('Method not implemented.');
+  // }
+  // setDisabledState?(isDisabled: boolean): void {
+  //   throw new Error('Method not implemented.');
+  // }
   ngAfterViewInit(): void {
-    throw new Error('Method not implemented.');
+    // const attrName = this.el.nativeElement.querySelector('input');
+    // if (this.qmAttribute !== '') attrName.setAttribute(this.qmAttribute, '');
   }
 
   ngOnInit(): void {
